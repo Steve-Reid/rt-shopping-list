912494c750c9dcfa645cc2d46989de9c797143b6
Steve Reid <steve@appby.design>
2017-07-07 17:35:12 +0100

    Refactored Editor component to make use of transform-class-properties plugin

--- imports/ui/Editor.jsx
+++ imports/ui/Editor.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Session } from 'meteor/session';
@@ -7,33 +7,13 @@ import { browserHistory } from 'react-router';
 
 import { Notes } from './../api/notes';
 
-export class Editor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleBodyChange = this.handleBodyChange.bind(this);
-    this.handleTitleChange = this.handleTitleChange.bind(this);
-    this.handleDeleteNote = this.handleDeleteNote.bind(this);
-    this.state = {
-      title: '',
-      body: ''
-    };
-  }
-  handleTitleChange(e) {
-    const title = e.target.value;
-    this.setState({ title });
-    this.props.call('notes.update', this.props.note._id, { title });
-  }
-  handleBodyChange(e) {
-    const body = e.target.value;
-    this.setState({ body });
-    this.props.call('notes.update', this.props.note._id, { body });
-  }
+export class Editor extends Component {
+  state = {
+    title: '',
+    body: ''
+  };
 
-  handleDeleteNote(e) {
-    this.props.call('notes.remove', this.props.note._id);
-    this.props.browserHistory.push('/dashboard');
-  }
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate = (prevProps, prevState) => {
     const currentNoteId = this.props.note ? this.props.note._id : undefined;
     const prevNoteId = prevProps.note ? prevProps.note._id : undefined;
 
@@ -44,6 +24,24 @@ export class Editor extends React.Component {
       });
     }
   }
+
+  handleBodyChange = (e) => {
+    const body = e.target.value;
+    this.setState({ body });
+    this.props.call('notes.update', this.props.note._id, { body });
+  }
+
+  handleDeleteNote = (e) => {
+    this.props.call('notes.remove', this.props.note._id);
+    this.props.browserHistory.push('/dashboard');
+  }
+
+  handleTitleChange = (e) => {
+    const title = e.target.value;
+    this.setState({ title });
+    this.props.call('notes.update', this.props.note._id, { title });
+  }
+
   render() {
     if (this.props.note) {
       return (
